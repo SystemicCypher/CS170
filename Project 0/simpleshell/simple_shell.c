0e015bc5ff117082c84377c2158729c5a2f2bffb
Ziliang Zeng <zzeng@umail.ucsb.edu>
2018-04-18 21:47:45 -0700

    fixed <

--- Project 0/simpleshell/simple_shell.c
+++ Project 0/simpleshell/simple_shell.c
@@ -25,61 +25,63 @@
 void runcommand(char* command, char** args) {
   pid_t pid = fork();
   if(pid) { // parent
-    	waitpid(pid, NULL, 0);
+    waitpid(pid, NULL, 0);
   } else { // child
-      int newfd;
-      for(int i = 0; args[i] != 0; i++) {
-        if(strcmp(args[i], "<") == 0) {
-          if((newfd = open(args[i+1], O_CREAT|O_RDONLY, 0644)) < 0) {
-            perror(args[i+1]);
-            exit(1);
-          }
-          close(0);
-          dup2(newfd, 0);
-          close(newfd);
-          for(int j = i; args[j] != 0; j++)
-            args[j] = args[j+2];
-        }
-        if(strcmp(args[i], ">") == 0) {
-          if((newfd = open(args[i+1], O_CREAT|O_WRONLY, 0644)) < 0) {
-            perror(args[i+1]);
-            exit(1);
-          }
-          close(1);
-          dup2(newfd, 1);
-          close(newfd);
-          for(int j = i; args[j] != 0; j++)
-            args[j] = args[j+2];
-        }
+    int newfd;
+    for(int i = 0; args[i] != 0; i++) {
+      if(strcmp(args[i], "<") == 0) {
+	if((newfd = open(args[i+1], O_CREAT|O_RDONLY, 0644)) < 0) {
+	  perror(args[i+1]);
+	  exit(1);
+	}
+	close(0);
+	dup2(newfd, 0);
+	close(newfd);
+	for(int j = i; args[j] != 0; j++)
+	  args[j] = args[j+2];
+	i--;
       }
-    	execvp(command, args);
+      else if(strcmp(args[i], ">") == 0) {
+	if((newfd = open(args[i+1], O_CREAT|O_WRONLY, 0644)) < 0) {
+	  perror(args[i+1]);
+	  exit(1);
+	}
+	close(1);
+	dup2(newfd, 1);
+	close(newfd);
+	for(int j = i; args[j] != 0; j++)
+	  args[j] = args[j+2];
+	i--;
+      }
+    }
+    execvp(command, args);
   }
 }
 
 int main(){
-    char line[MAX_LINE_LENGTH];
-    //printf("shell: ");
-    while(fgets(line, MAX_LINE_LENGTH, stdin)) {
-    	// Build the command and arguments, using execv conventions.
-    	line[strlen(line)-1] = '\0'; // get rid of the new line
-    	char* command = NULL;
-    	char* arguments[MAX_TOKEN_COUNT];
-    	int argument_count = 0;
-
-    	char* token = strtok(line, " ");
-    	while(token) {
-      		if(!command) command = token;
-      		arguments[argument_count] = token;
-	      	argument_count++;
-      		token = strtok(NULL, " ");
-    	}
-    	arguments[argument_count] = NULL;
-	if(argument_count>0){
-		if (strcmp(arguments[0], "exit") == 0)
-            		exit(0);
-    		runcommand(command, arguments);
-	}
-        //printf("shell: ");
+  char line[MAX_LINE_LENGTH];
+  //printf("shell: ");
+  while(fgets(line, MAX_LINE_LENGTH, stdin)) {
+    // Build the command and arguments, using execv conventions.
+    line[strlen(line)-1] = '\0'; // get rid of the new line
+    char* command = NULL;
+    char* arguments[MAX_TOKEN_COUNT];
+    int argument_count = 0;
+    
+    char* token = strtok(line, " ");
+    while(token) {
+      if(!command) command = token;
+      arguments[argument_count] = token;
+      argument_count++;
+      token = strtok(NULL, " ");
     }
-    return 0;
+    arguments[argument_count] = NULL;
+    if(argument_count>0){
+      if (strcmp(arguments[0], "exit") == 0)
+	exit(0);
+      runcommand(command, arguments);
+    }
+    //printf("shell: ");
+  }
+  return 0;
 }
