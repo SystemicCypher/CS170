834b9d38a0ce795533806696816377ceaa038d26
zzeng <zzeng@umail.ucsb.edu>
2018-04-18 18:46:27 -0700

    < and > should work correctly tested on testfile2

--- Project 0/simpleshell/simple_shell.c
+++ Project 0/simpleshell/simple_shell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #define MAX_TOKEN_LENGTH 50
 #define MAX_TOKEN_COUNT 100
@@ -10,7 +11,7 @@
 
 // Simple implementation for Shell command
 // Assume all arguments are seperated by space
-// Erros are ignored when executing fgets(), fork(), and waitpid(). 
+// Erros are ignored when executing fgets(), fork(), and waitpid().
 
 /**
  * Sample session
@@ -26,13 +27,38 @@ void runcommand(char* command, char** args) {
   if(pid) { // parent
     	waitpid(pid, NULL, 0);
   } else { // child
+      int newfd;
+      for(int i = 0; args[i] != 0; i++) {
+        if(strcmp(args[i], "<") == 0) {
+          if((newfd = open(args[i+1], O_CREAT|O_RDONLY, 0644)) < 0) {
+            perror(args[i+1]);
+            exit(1);
+          }
+          close(0);
+          dup2(newfd, 0);
+          close(newfd);
+          for(int j = i; args[j] != 0; j++)
+            args[j] = args[j+2];
+        }
+        if(strcmp(args[i], ">") == 0) {
+          if((newfd = open(args[i+1], O_CREAT|O_WRONLY, 0644)) < 0) {
+            perror(args[i+1]);
+            exit(1);
+          }
+          close(1);
+          dup2(newfd, 1);
+          close(newfd);
+          for(int j = i; args[j] != 0; j++)
+            args[j] = args[j+2];
+        }
+      }
     	execvp(command, args);
   }
 }
 
 int main(){
     char line[MAX_LINE_LENGTH];
-    //printf("shell: "); 
+    //printf("shell: ");
     while(fgets(line, MAX_LINE_LENGTH, stdin)) {
     	// Build the command and arguments, using execv conventions.
     	line[strlen(line)-1] = '\0'; // get rid of the new line
@@ -53,7 +79,7 @@ int main(){
             		exit(0);
     		runcommand(command, arguments);
 	}
-        //printf("shell: "); 
+        //printf("shell: ");
     }
     return 0;
 }
