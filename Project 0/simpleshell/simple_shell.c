98bf0bec802f217d5696f0a522a87cb4f120be2e
zzeng <zzeng@umail.ucsb.edu>
2018-04-20 14:42:12 -0700

    added comments

--- Project 0/simpleshell/simple_shell.c
+++ Project 0/simpleshell/simple_shell.c
@@ -22,19 +22,26 @@
  *   Makefile  simple  simple_shell.c
  *   shell: exit
 **/
+
+// Function prototypes
 void runcommandwithpipe(char* command1, char* command2, char** args1, char** args2, int pfd[]);
 void runcommand(char* command, char** args);
 
+// Helper function for a command that has pipes within them
 void runcommandwithpipe(char* command1, char* command2, char** args1, char** args2, int pfd[]) {
+  // Forks the parent will run the second command and the child will run the first command
+  // We want the first command to run first so that the output will be given to the input of the second command
   pid_t pid = fork();
 
   if(pid) { // parent
+    // Wait for child to finish running its command and then queues the second command to runcommand after correctly assigning file descriptors
     waitpid(pid, NULL, 0);
     dup2(pfd[0], 0);
     close(pfd[0]);
     close(pfd[1]);
     runcommand(command2, args2);
   } else { //child
+    // Correctly assigns file descriptors and queues its command to runcommand. After run command finishs, exits so the parent can run.
     dup2(pfd[1], 1);
     close(pfd[0]);
     close(pfd[1]);
@@ -44,9 +51,11 @@ void runcommandwithpipe(char* command1, char* command2, char** args1, char** arg
 }
 
 void runcommand(char* command, char** args) {
+  // Flag for whether there is a pipe and first to find the first pipe
   int flag = 0;
   int first = 0;
 
+  // Initialize command1, command2, args1, args2 if we need to split the commands into two commands in the case when there is a pipe present in the command
   char* command1 = NULL;
   char* command2 = NULL;
   char* args1[MAX_TOKEN_COUNT];
@@ -54,6 +63,7 @@ void runcommand(char* command, char** args) {
   bzero(args1, MAX_TOKEN_COUNT);
   bzero(args2, MAX_TOKEN_COUNT);
 
+  // Finds a pipe if it exists and splits command and args into command1, command2, args1, args2. Sets flag to 1 and first to 1 if pipe is found
   for(int i = 0; args[i] != 0; i++) {
     if(strcmp(args[i], "|") == 0) {
       if(first == 0) {
@@ -71,6 +81,8 @@ void runcommand(char* command, char** args) {
     }
   }
   if(flag) {
+    // If pipe exists we run pipe on a file descriptor to get file descriptors for input and output
+    // Fork so the child runs the helper function for commands with pipes and the parent will wait for the child to finish
     pid_t pipeid = fork();
 
     int pipefd[2];
@@ -84,6 +96,9 @@ void runcommand(char* command, char** args) {
     }
   }
   else {
+    // If pipe does not exist in the command, we check if there is input/output redirection
+    // If we find input/output redirection, we change the appropriate file descriptors and correctly change the arguments list
+    // Then call execvp on the command
     pid_t pid = fork();
     if(pid) { // parent
       waitpid(pid, NULL, 0);
@@ -120,6 +135,7 @@ void runcommand(char* command, char** args) {
   }
 }
 
+// Signal handler function to handle when ctrl-z is pressed. If pressed twice, the process will stop.
 void handler(int sig) {
   static int count = 0;
   printf("Enter CTRL-Z again to exit\n");
@@ -129,6 +145,7 @@ void handler(int sig) {
 }
 
 int main(){
+  // Changes the default SIGTSTP behavior to our custom handler
   signal(SIGTSTP, handler);
 
   char line[MAX_LINE_LENGTH];
