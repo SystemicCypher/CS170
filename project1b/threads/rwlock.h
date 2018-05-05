98302bd09f467f4a83ed892a4d68b1a426689eab
Ziliang Zeng <zzeng@umail.ucsb.edu>
2018-05-04 18:18:37 -0700

    added rwlock code to project1b

--- project1b/threads/rwlock.h
+++ project1b/threads/rwlock.h
@@ -1,19 +1,21 @@
 #ifndef __RWLOCK_H__
 #define __RWLOCK_H__
 #include<semaphore.h>
-#include "synch.h"
 
 class RWLock{
 private:
+  int AR, WR, AW, WW;
+  pthread_cond_t okToRead, okToWrite;
+  pthread_mutex_t lock;
 public:
-    	RWLock();
-    	~RWLock();
+    RWLock();
+    ~RWLock();
     //Reader
-    	void startRead();
-    	void doneRead();
+    void startRead();
+    void doneRead();
     // Writer
-    	void startWrite();
-    	void  doneWrite();
+    void startWrite();
+    void  doneWrite();
 };
 
 #endif
